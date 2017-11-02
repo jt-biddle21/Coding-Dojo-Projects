5ae30a506abf3fca069b38cce531dc57be4a0de5
Justin Biddle <justinbiddle@Justins-MacBook-Air.local>
2017-11-01 21:19:47 -0700

    Fourteenth Commit

--- Python/FlaskFundamentals/DojoSurvey/surveryServer.py
+++ Python/FlaskFundamentals/DojoSurvey/surveryServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -9,14 +9,16 @@ def homepage():
 
 @app.route("/results", methods=["POST"])
 def resultspage():
-
-    return render_template("results.html", name=request.form["yourname"])
+    name = request.form["name"]
+    location = request.form["location"]
+    lang = request.form["lang"]
+    comment = request.form["comment"]
+    return render_template("results.html", name=request.form['name'], location=request.form["location"], lang=request.form["lang"], comment=request.form['comment'])
 
 
 @app.route("/dojoform")
 def formpage():
     return render_template("form.html")
-    return redirect("/")
 
 
 app.run(debug=True)
