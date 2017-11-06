ccb348458ef49fb736c507384a95ea2ba400dc95
Justin Biddle <justinbiddle@Justins-MacBook-Air.local>
2017-11-06 08:25:41 -0800

    Finished numbergame and made changed to it

--- Python/FlaskFundamentals/NumberGame/numberGameServer.py
+++ Python/FlaskFundamentals/NumberGame/numberGameServer.py
@@ -1,28 +1,35 @@
+import random
+
 from flask import Flask, render_template, request, session, redirect
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
 
-def setrandom():
-    import random
-    x = random.randint(0, 101)
-    session['randnumber'] = x
-    return x
-
-
-def resetrandom():
-    session.pop('randnumber')
-
-
 @app.route('/')
-def initialpage():
+def index():
+    session['number'] = random.randrange(0, 100)
     return render_template('index.html')
 
 
-@app.route('/guess', methods=["POST"])
-def storedata():
-    session["number"] = request.form["number"]
-    return redirect('/')
+@app.route('/guess', methods=['POST'])
+def guess():
+    if not session.get('number'):
+        return redirect('/')
+    answer = None
+    try:
+        user_guess = int(request.form['guess'])
+    except ValueError:
+        bad_guess = request.form['guess'] if request.form['guess'] else "No guess at all"
+        answer = "{} is not a number. Please try again.".format(bad_guess)
+        return render_template("index.html", answer=answer)
+    if user_guess > session["number"]:
+        answer = "Too High"
+    if user_guess < session['number']:
+        answer = "Too Low"
+    if answer is None:
+        answer = 'Correct. You guessed {}. Click submit to play again!'.format(user_guess, session['number'])
+        session.pop('number')
+    return render_template("index.html", answer=answer)
 
 
 app.run(debug=True)
--- Python/Fundamentals/makingTuples.py
+++ Python/Fundamentals/makingTuples.py
@@ -8,16 +8,28 @@ my_dict = {
 # [("Speros", "(555) 555-5555"), ("Michael", "(999) 999-9999"), ("Jay", "(777) 777-7777")]
 
 
-def maketuples(x):
-    newtuple = ()
-    for z in x:
+def maketuples(x_dict):
+    # newtuple = ()
+    for z in x_dict:
         print z
         newtuple = newtuple + (x[z])
+
     for i in my_dict.itervalues():
         print i
+
     newtuple = newtuple + (x[z])
     print newtuple
 
 
+def tuple_from_dict(a_dict):
+    rv = ()
+    for k, v in a_dict.iteritems():
+        rv += k, v,
+    return rv
 
-maketuples(my_dict)
+
+# maketuples(my_dict)
+new_tuple = tuple_from_dict(my_dict)
+
+print (new_tuple)
+print(type(new_tuple))
