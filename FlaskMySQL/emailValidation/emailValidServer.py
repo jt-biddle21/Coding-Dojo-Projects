5c6777005e38325a36e2851a671b7fd3e57e8363
Justin Biddle <justinbiddle@Justins-MacBook-Air.local>
2017-11-07 14:44:42 -0800

    added finishing touches to emailValidate

--- FlaskMySQL/emailValidation/emailValidServer.py
+++ FlaskMySQL/emailValidation/emailValidServer.py
@@ -10,9 +10,14 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 @app.route('/')
 def index():
+    return render_template('emailValid.html')
+
+
+@app.route('/success')
+def success():
     get_all_emails_query = "SELECT * FROM emails"
     all_emails = mysql.query_db(get_all_emails_query)
-    return render_template('emailValid.html', email=all_emails)
+    return render_template('success.html', bemail=all_emails)
 
 
 @app.route('/validate', methods=["POST"])
@@ -20,13 +25,15 @@ def validate():
     session['email'] = request.form['email']
     if len(session['email']) < 1:
         flash("Email cannot be blank!")
+        return redirect('/')
     elif not EMAIL_REGEX.match(session['email']):
         flash("Invalid Email Address!")
+        return redirect('/')
     else:
         add_email_query = "INSERT INTO emails (email) VALUES (:qemail)"
         data = {"qemail": request.form["email"]}
         mysql.query_db(add_email_query, data)
-    return redirect('/')
+    return redirect('/success')
 
 
 app.run(debug=True)
