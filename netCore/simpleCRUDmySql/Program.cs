10e2af783c9fc2f2129335dc43dafc8e8a175f26
Justin <jtbiddle17@gmail.com>
2017-12-01 10:41:11 -0800

    Added music-linq, and simple..., as well as firstWebApp

---
+++ netCore/simpleCRUDmySql/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DbConnection;
+
+namespace simpleCRUDmySql
+{
+    class Program
+    {
+        public void Read(string table)
+        {
+            List<Dictionary<string, object>> queryInfo = DbConnector.Query($"Select * from {table}");
+            foreach (Dictionary<string, object> tablesUsers in queryInfo)
+            {
+                foreach (KeyValuePair<string, object> usersContent in tablesUsers)
+                {
+                    Console.WriteLine($"{usersContent.Key}, {usersContent.Value}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop == true)
+            {
+                Console.WriteLine("Enter in a query:");
+                string que = Console.ReadLine();
+                List<Dictionary<string, object>> queryInfo = DbConnector.Query(que);
+                foreach (Dictionary<string, object> tablesUsers in queryInfo)
+                {
+                    foreach (KeyValuePair<string, object> usersContent in tablesUsers)
+                    {
+                        Console.WriteLine($"{usersContent.Key}, {usersContent.Value}");
+                    }
+                }
+                loop = false;
+            }
+            
+        }
+    }
+}
